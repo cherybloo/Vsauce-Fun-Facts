409e6bc091be988114ddac30d89dce88f42a66ad
cherybloo <laurenciomichael98@gmail.com>
2024-05-13 01:48:57 +0200

    Updated funfact.js and manifest.json

--- extensions/funfact.js
+++ extensions/funfact.js
@@ -1,1 +1,12 @@
-alert("Hey Vsauce, Michael here!")
+async function funfact(url) {
+    await fetch(url)
+    .then(res => res.json())
+    .then(fact => {
+        var vsauce = fact[Math.floor(Math.random()*Object.keys(fact).length)]
+        alert(vsauce)
+    })
+}
+
+//alert("hello from sound");
+
+funfact("https://raw.githubusercontent.com/cherybloo/Vsauce-Fun-Facts/main/funfact.json")
